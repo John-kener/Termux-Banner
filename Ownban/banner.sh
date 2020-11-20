54b436d286ead1bdcebda97d564e74bec51f6ab7
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-20 13:48:50 +0530

    Create banner.sh

---
+++ Ownban/banner.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+clear
+echo -e " \e[1;32m 
+#BANNER "
+echo
+echo -e " \e[1;33m                        CREATED BY JOHN KENER       "
+echo
+echo
+echo -e  " \e[1;37m ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄ "
+echo
+
+
+echo -e "\e[1;33m                 [1]  ඔයාගේ බැනර් එකට ASCIIARTS අරගන්න           ";  
+echo -e "\e[1;33m                 [2]  බැනර් එක හදාගන්න                                                 ";  
+echo
+echo
+echo
+read -p' OPTION එකක් තෝරන්න :- ' userinput1;
+if [ "${userinput1:-}" = "1" ]
+then
+  cd && cd Termux-Banner && cd Ownban && bash Asciiarts.sh
+fi
+if [ "${userinput1:-}" = "2" ]
+then
+       cd && cd Termux-Banner && cd Ownban && nano CW && figlet over |lolcat
+fi
+       
+echo
+cd /data/data/com.termux/files/usr/etc/
+rm -rf zshrc
+cd && cd Termux-Banner && cd Ownban && cp zshrc /data/data/com.termux/files/usr/etc/
+cat "CW" >> /data/data/com.termux/files/usr/etc/bash.bashrc
